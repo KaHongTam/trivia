ad5c1b8847fd72885652f63c80ffa493bf597b13
KaHongTam <kahongtam@hotmail.com>
2018-12-18 14:02:34 +0100

    meerdere vragen tonen en divs show en hide nadat antwoord gegeven is

--- library.js
+++ library.js
@@ -1,4 +1,6 @@
-fetch('https://opentdb.com/api.php?amount=1&category=18&difficulty=medium&type=boolean')
+var questionData;
+
+fetch('https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=boolean')
   .then(
     function(response) {
       if (response.status !== 200) {
@@ -9,13 +11,14 @@ fetch('https://opentdb.com/api.php?amount=1&category=18&difficulty=medium&type=b
 
       // Examine the text in the response
       response.json().then(function(data) {
-        // console.log(data);
-        // console.log(data.results);
-        // console.log(data.results[0].question);
-        // console.log(data.results[0].correct_answer);
-        document.getElementById("question").innerHTML = data.results[0].question;
-        document.getElementById("correctAnswer").innerHTML = data.results[0].correct_answer;
-        var correct_answer = data.results[0].correct_answer;
+        i = 0;
+        questionData = data;
+        document.getElementById("currentQuestionNumber").innerHTML = i + 1;
+        document.getElementById("totalQuestionNumber").innerHTML = questionData.results.length;
+        document.getElementById("question").innerHTML = data.results[i].question;
+        document.getElementById("correctAnswer").innerHTML = data.results[i].correct_answer;
+        question = document.getElementById("question").innerHTML;
+        correct_answer = data.results[i].correct_answer;
       });
     }
   )
@@ -24,11 +27,20 @@ fetch('https://opentdb.com/api.php?amount=1&category=18&difficulty=medium&type=b
   });
 
 function displayAnswer(answer) {
-    document.getElementById("result").innerHTML = answer;
+  $("#finalAnswer").show();
+  $("#noAnswer").hide();
+  $("#submitButton").prop('disabled', false);
+  document.getElementById("result").innerHTML = answer;
 }
 
 function checkAnswer() {
+  if (document.getElementById("checker1").checked == false &&  document.getElementById("checker2").checked == false) {
+    $("#noAnswer").show();
+    $("#submitButton").prop('disabled', true);
+  }
+  else {
     var checker = document.getElementById("result").innerHTML;
+    $("#showAnswer").show();
     if (checker == document.getElementById("correctAnswer").innerHTML) {
         document.getElementById("showAnswer").innerHTML = "You answered correctly, congratulations!";
     }
@@ -36,5 +48,28 @@ function checkAnswer() {
         document.getElementById("showAnswer").innerHTML = "Alas, this is the wrong answer, try again!";
     }
     $("input[type=radio]").attr('disabled', true);
+    $("#submitButton").hide();
+    $("#nextQuestionButton").show();
+    if (i + 1 == questionData.results.length) {
+      // window.open("https://www.google.nl","_self");
+      $("#nextQuestionButton").hide();
+      $("#scoreBoardButton").show();
+    }
+  }
 }
 
+function nextQuestion() {
+    i++;
+    document.getElementById("question").innerHTML = questionData.results[i].question;
+    document.getElementById("correctAnswer").innerHTML = questionData.results[i].correct_answer;
+    document.getElementById("currentQuestionNumber").innerHTML = i + 1;
+    $("#submitButton").show();
+    $("#nextQuestionButton").hide();
+    $("#finalAnswer").hide();
+    $("#showAnswer").hide();
+    document.getElementById("checker1").checked = false;
+    document.getElementById("checker2").checked = false;
+    $("input[type=radio]").attr('disabled', false);
+    
+  console.log ("i=", i , "totaal aantal vragen=",  questionData.results.length);
+}
